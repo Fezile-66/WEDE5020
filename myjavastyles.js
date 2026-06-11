c3cf66dc5e27b9ec442427a0a6d638f1eeb76db4
Fezile-66 <st10486745@rcconnect.edu.za>
2026-06-11 13:09:01 +0200

    Add files via upload

---
+++ myjavastyles.js
@@ -0,0 +1,5 @@
+lightbox.option({
+    'resizeDuration': 200, //image resizing animation takes 0.2 seconds
+    'wrapAround': true, // Last image links back to the first images
+    'fadeDuratio' : 300 //Fade effect takes 0.3 seconds
+})
